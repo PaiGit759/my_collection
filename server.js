6fa465bff94dad5c7036969fd18cb210a22a55c3
PaiGit759 <107637697+PaiGit759@users.noreply.github.com>
2025-12-21 12:31:22 +0100

    20251221 7

--- server.js
+++ server.js
@@ -1,96 +1,157 @@
+// const express = require("express");
+// const path = require('path');
+
+// const bodyParser = require("body-parser");
+// const mongoose = require('mongoose');
+
+// const createPath = require('./helpers/create-path');
+// const { getUsers, user, curuser, edituser } = require('./controllers/authController.js');
+
+// const { getallcollection, getgallerycount, getgallerypage, collection, editcollection, deletecollection } = require('./controllers/collectionController.js');
+
+// const User = require('./models/userModel.js');
+
+// require('dotenv').config();
+
+// const app = express();
+
+// app.set('view engine', 'ejs');
+
+// const authRouter = require('./routes/authRoutes.js');
+// const collectionRoutes = require('./routes/collectionRoutes.js');
+
+// app.use(bodyParser.json());
+
+// app.use(express.urlencoded({ extended: false }));
+
+// app.use(express.static(path.join(__dirname, 'public')));
+
+// app.set('views', path.join(__dirname, 'views'));
+
+// mongoose.connect(process.env.MONGO_URL,)
+//   .then(() => console.log('Connected to MongoDB Atlas'))
+//   .catch(err => console.error('MongoDB connection error:', err));
+
+
+// app.use('/api', authRouter);
+// app.use('/api', collectionRoutes);
+
+// app.get("/", (req, res) => {
+//   const title = 'My collection';
+//   const user = null;
+//   res.render(createPath('index'), { title, user });
+// });
+
+// app.get("/getUsers", (req, res) => {
+//   const title = 'My collection';
+// });
+
+// app.get("/users", (req, res) => {
+//   const title = 'My collection';
+// });
+
+// app.get('/user', user);
+
+// //app.get('/edituser', edituser);
+
+// app.get('/edituser', edituser);
+
+// app.get('/editcollection', editcollection);
+
+// app.get('/deletecollection', deletecollection);
+
+
+// app.get("/login", (req, res) => {
+//   const title = 'Login';
+//   const user = 'User is undefined';
+//   res.render(createPath('login'), { title, user });
+// });
+
+// app.get("/register", (req, res) => {
+//   const title = 'Register';
+//   const user = 'User is undefined';
+//   const message = '*********';
+//   res.render(createPath('register'), { message, user });
+// });
+
+// app.get("/addcollection", (req, res) => {
+//   const title = 'Collection';
+//   res.render(createPath('addcollection'));
+// });
+
+// app.get("/about", (req, res) => {
+//   const title = 'About';
+//   res.render(createPath('about'));
+// });
+
+// app.get('/gallery', getallcollection);
+
+// app.get('/gallery/count', getgallerycount);
+
+// app.get('/gallerypage', getgallerypage);
+
+// app.get('/collection', collection);
+
+// const port = process.env.PORT || 3000;
+// app.listen(port, () => console.log(`listening port http://localhost:${process.env.PORT}`));
+
 const express = require("express");
 const path = require('path');
-
-const bodyParser = require("body-parser");
 const mongoose = require('mongoose');
+require('dotenv').config();
 
 const createPath = require('./helpers/create-path');
 const { getUsers, user, curuser, edituser } = require('./controllers/authController.js');
-
 const { getallcollection, getgallerycount, getgallerypage, collection, editcollection, deletecollection } = require('./controllers/collectionController.js');
-
-const User = require('./models/userModel.js');
-
-require('dotenv').config();
-
-const app = express();
-
-app.set('view engine', 'ejs');
 
 const authRouter = require('./routes/authRoutes.js');
 const collectionRoutes = require('./routes/collectionRoutes.js');
 
-app.use(bodyParser.json());
+const app = express();
 
-app.use(express.urlencoded({ extended: false }));
-
-app.use(express.static(path.join(__dirname, 'public')));
-
+app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
-mongoose.connect(process.env.MONGO_URL,)
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+mongoose.connect(process.env.MONGO_URL)
   .then(() => console.log('Connected to MongoDB Atlas'))
   .catch(err => console.error('MongoDB connection error:', err));
-
 
 app.use('/api', authRouter);
 app.use('/api', collectionRoutes);
 
 app.get("/", (req, res) => {
-  const title = 'My collection';
-  const user = null;
-  res.render(createPath('index'), { title, user });
-});
-
-app.get("/getUsers", (req, res) => {
-  const title = 'My collection';
-});
-
-app.get("/users", (req, res) => {
-  const title = 'My collection';
+  res.render(createPath('index'), { title: 'My collection', user: null });
 });
 
 app.get('/user', user);
-
 app.get('/edituser', edituser);
-
-app.get('/edituser', edituser);
-
 app.get('/editcollection', editcollection);
-
 app.get('/deletecollection', deletecollection);
 
-
 app.get("/login", (req, res) => {
-  const title = 'Login';
-  const user = 'User is undefined';
-  res.render(createPath('login'), { title, user });
+  res.render(createPath('login'), { title: 'Login', user: null });
 });
 
 app.get("/register", (req, res) => {
-  const title = 'Register';
-  const user = 'User is undefined';
-  const message = '*********';
-  res.render(createPath('register'), { message, user });
+  res.render(createPath('register'), { title: 'Register', user: null, message: '*********' });
 });
 
 app.get("/addcollection", (req, res) => {
-  const title = 'Collection';
-  res.render(createPath('addcollection'));
+  res.render(createPath('addcollection'), { title: 'Collection' });
 });
 
 app.get("/about", (req, res) => {
-  const title = 'About';
-  res.render(createPath('about'));
+  res.render(createPath('about'), { title: 'About' });
 });
 
 app.get('/gallery', getallcollection);
-
 app.get('/gallery/count', getgallerycount);
-
 app.get('/gallerypage', getgallerypage);
-
 app.get('/collection', collection);
 
 const port = process.env.PORT || 3000;
-app.listen(port, () => console.log(`listening port http://localhost:${process.env.PORT}`));
+app.listen(port, () => console.log(`Server running on port http://localhost:${port}`));
